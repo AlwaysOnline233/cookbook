c7fc4f198f808e564a3f7f4bb7e85cb1d5318d7e
gs <941906145@qq.com>
2020-05-10 23:54:56 +0800

    small bug fix

--- chapter_1/1_3.py
+++ chapter_1/1_3.py
@@ -1,24 +1,24 @@
 # 保留最后N个元素
-
+#
 from collections import deque    # collections.deque保留有限历史记录
 
 
-# def search(lines, pattern, history=5):
-#     previous_lines = deque(maxlen=history)
-#     for line in lines:
-#         if pattern in line:
-#             yield line, previous_lines           # yield函数，生成器
-#         previous_lines.append(line)
-#
-#
-# # .表示为当前路径  ..表示为当前路径的父级目录
-# if __name__ == '__main__':
-#     with open(r'somefile.docx') as f:
-#         for line, prevlines in search(f, 'python', 5):
-#             for pline in prevlines:
-#                 print(pline, end='')
-#             print(line, end='')
-#             print('-' * 20)
+def search(lines, pattern, history=5):
+    previous_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield line, previous_lines           # yield函数，生成器
+        previous_lines.append(line)
+
+
+# .表示为当前路径  ..表示为当前路径的父级目录
+if __name__ == '__main__':
+    with open('somefile.txt') as f:
+        for line, prevlines in search(f, 'python', 5):
+            for pline in prevlines:
+                print(pline, end='')
+            print(line, end='')
+            print('-' * 20)
 
 
 # 使用deque(maxlen=N)构造函数会新建一个固定大小的队列，当新元素加入且这个队列已满，最老的元素自动被移除

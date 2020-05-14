34188674b2c113d4db2458cef72f6414b06f0622
gs <941906145@qq.com>
2020-05-14 23:14:42 +0800

    small bug fix

--- chapter_1/1_18.py
+++ chapter_1/1_18.py
@@ -15,3 +15,37 @@ addr, joined = sub
 print(addr)
 print(joined)
 
+print('——————————————————————')
+Stock = namedtuple('Stock', ['name', 'shares', 'price'])
+def compute_cost(records):
+    total = 0.0
+    for rec in records:
+        s = Stock(*rec)
+        total += s.shares * s.price
+    return total
+
+x = compute_cost([('xacker',0.2,80)])
+print(x)
+print('———————————————————————————————')
+print('_replace()方法，它会创建一个全新的命名元组并将对应的字段用新的值取代')
+s = Stock('ACME', 100, 123.45)
+s = s._replace(shares=75)
+print(s)
+
+print('———————————————————————————————')
+print('当命名元组拥有可选或者缺失字段时候，_replace()是一个方便的填充数据d的方法')
+print('先创建一个包含缺省值的原型元组，然后使用 _replace()创建新的值被更新过的实例')
+Stock = namedtuple('Stock', ['name', 'shares', 'price', 'date', 'time'])
+
+# Create a prototype instance
+stock_prototype = Stock('', 0, 0.0, None, None)
+# Function to convert a dictionary to a Stock
+def dict_to_stock(s):
+    return stock_prototype._replace(**s)
+
+a = {'name': 'ACME', 'shares': 100, 'price': 123.45}
+x = dict_to_stock(a)
+b = {'name': 'ACME', 'shares': 100, 'price': 123.45, 'date': '12/17/2012'}
+y = dict_to_stock(b)
+print(x)
+print(y)
--- chapter_1/1_19.py
+++ chapter_1/1_19.py
@@ -2,22 +2,23 @@
 import os
 
 # 使用生成器表达式参数
+print('计算平方和')
 nums = [1, 2, 3, 4, 5]
 # s = sum((x * x for x in nums))    # 显式的传递一个生成器表达式对象
 s = sum(x * x for x in nums)        # 省略了括号
 print(s)
 
 print('—————————————example—————————————————')
-# Determine if any .py files exist in a directory
-files = os.listdir('../chapter_1')
+# 1) Determine if any .py files exist in a directory
+files = os.listdir('../chapter_1')             # 路径
 if any(name.endswith('.py') for name in files):
     print('There be python!')
 else:
     print('Sorry, no python.')
-# Output a tuple as CSV
+# 2) Output a tuple as CSV
 s = ('ACME', 50, 123.45)
 print(','.join(str(x) for x in s))
-# Data reduction across fields of a data structure
+# 3) Data reduction across fields of a data structure
 portfolio = [
     {'name':'GOOG', 'shares': 50},
     {'name':'YHOO', 'shares': 75},
@@ -27,3 +28,10 @@ portfolio = [
 min_shares = min(s['shares'] for s in portfolio)
 print(min_shares)
 
+print('key 关键字参数')
+# Original: Returns 20
+min_shares = min(s['shares'] for s in portfolio)
+print(min_shares)
+# Alternative: Returns {'name': 'AOL', 'shares': 20}
+min_shares = min(portfolio, key=lambda s: s['shares'])
+print(min_shares)

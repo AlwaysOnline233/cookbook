0c6f83f11bfedf059299fe76ed4ef34c9424cd00
gs <941906145@qq.com>
2020-06-01 23:55:57 +0800

    Initial commit

---
+++ chapter_7/7_9.py
@@ -0,0 +1,16 @@
+# 将单方法的类转换为函数
+
+from urllib.request import urlopen
+
+
+class UrlTemplate:
+    def __init__(self, template):
+        self.template = template
+
+    def open(self, **kwargs):
+        return urlopen(self.template.format_map(kwargs))
+
+# Example use. Download stock data from yahoo
+yahoo = UrlTemplate('http://finance.yahoo.com/d/quotes.csv?s={names}&f={fields}')
+for line in yahoo.open(names='IBM,AAPL,FB', fields='sl1c1v'):
+    print(line.decode('utf-8'))

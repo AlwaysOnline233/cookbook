f68ca131e36080e891cd1a1693a9374a20fd1ccb
gs <941906145@qq.com>
2020-07-03 11:16:37 +0800

    Initial commit

---
+++ leetcode/108.py
@@ -0,0 +1,40 @@
+# 将有序数组转换为二叉搜索树
+'''
+将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+示例:
+给定有序数组: [-10,-3,0,5,9],
+一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+
+来源：力扣（LeetCode）
+'''
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def sortedArrayToBST(self, nums):
+        def helper(left, right):
+            if left > right:
+                return None
+
+            # 总是选择中间位置左边的数字作为根节点
+            mid = (left + right) // 2
+
+            root = TreeNode(nums[mid])
+            root.left = helper(left, mid - 1)
+            root.right = helper(mid + 1, right)
+            return root
+
+        return helper(0, len(nums) - 1)

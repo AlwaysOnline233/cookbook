89d50bb791052688e1a002e349de3606602ff34e
gs <941906145@qq.com>
2020-07-16 09:49:13 +0800

    Initial commit

---
+++ leetcode/75.py
@@ -0,0 +1,39 @@
+# 颜色分类
+'''
+给定一个包含红色、白色和蓝色，一共n个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+注意:不能使用代码库中的排序函数来解决这道题。
+示例: 输入: [2,0,2,1,1,0]      输出: [0,0,1,1,2,2]
+
+进阶：
+一个直观的解决方案是使用计数排序的两趟扫描算法。
+首先，迭代计算出0、1 和 2 元素的个数，然后按照0、1、2的排序，重写当前数组。
+能想出一个仅使用常数空间的一趟扫描算法吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sort-colors
+'''
+
+# 用三个指针（p0, p2 和curr）来分别追踪0的最右边界，2的最左边界和当前考虑的元素
+# 沿着数组移动 curr 指针，若nums[curr] = 0，则将其与 nums[p0]互换；若 nums[curr] = 2 ，则与 nums[p2]互换
+class Solution:
+    def sortColors(self, nums):
+        '''
+        荷兰三色旗问题解
+        '''
+        # 对于所有 idx < p0 : nums[idx < p0] = 0
+        # curr是当前考虑元素的下标
+        p0 = curr = 0
+        # 对于所有 idx > p2 : nums[idx > p2] = 2
+        p2 = len(nums) - 1
+        while curr <= p2:
+            if nums[curr] == 0:
+                nums[p0], nums[curr] = nums[curr], nums[p0]
+                p0 += 1
+                curr += 1
+            elif nums[curr] == 2:
+                nums[curr], nums[p2] = nums[p2], nums[curr]
+                p2 -= 1
+            else:
+                curr += 1
+

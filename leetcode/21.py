6578555a6c83f04d7478a9972caeb4dc224d0e45
gs <941906145@qq.com>
2020-09-03 22:12:04 +0800

    合并两个有序链表

---
+++ leetcode/21.py
@@ -0,0 +1,26 @@
+# 合并两个有序链表
+'''
+将两个升序链表合并为一个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+示例：
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+'''
+
+
+class Solution:
+    # 递归
+    def mergeTwoLists(self, l1, l2):
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
